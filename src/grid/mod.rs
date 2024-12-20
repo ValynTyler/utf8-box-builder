10159752ef1f150361acb16f9d0bd49982ae9d6b
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-20 17:42:36 +0200

    feat: impl debug for grid

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,8 +1,38 @@
+use core::fmt::Debug;
 use std::fmt::Display;
 
 use crate::*;
 
 pub struct Grid<T>(pub Vec<Vec<T>>);
+
+impl<T> Debug for Grid<T>
+where T: Debug {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // top row
+        write!(f, "{TLC}{HL}")?;
+        let res: Result<Vec<_>, _> = self.0[0].iter().map(|_| write!(f, "{HL}{HL}")).collect();
+        res?;
+        writeln!(f, "{TRC}")?;
+
+        // middle rows
+        let _: Result<Vec<_>, _> = self.0.iter().map(|row| {
+            write!(f, "{VL} ")?;
+            let res: Result<Vec<_>, _> = row.iter().map(|item| {
+                write!(f, "{:?} ", item)
+            }).collect();
+            res?;
+            writeln!(f, "{VL}")
+        }).collect();
+
+        // bottom row
+        write!(f, "{BLC}{HL}")?;
+        let res: Result<Vec<_>, _> = self.0[0].iter().map(|_| write!(f, "{HL}{HL}")).collect();
+        res?;
+        write!(f, "{BRC}")?;
+
+        Ok(())
+    }
+}
 
 impl<T> Display for Grid<T>
 where T: Display {
