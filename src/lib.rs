ab3ce63e05b9a78fee92557b0737599142c0f12b
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-06 23:11:07 +0200

    feat: nineslice module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use nine_slice::NineSlice;
+
 pub mod grid;
+pub mod nine_slice;
 
 pub const HL: char = '─';
 pub const VL: char = '│';
@@ -7,3 +10,15 @@ pub const TL: char = '┌';
 pub const BL: char = '└';
 pub const TR: char = '┐';
 pub const BR: char = '┘';
+
+pub const UTF_BOX: NineSlice = NineSlice {
+    _1: '┌',
+    _2: '─',
+    _3: '┐',
+    _4: '│',
+    _5: ' ',
+    _6: '│',
+    _7: '└',
+    _8: '─',
+    _9: '┘',
+};
---
+++ src/nine_slice/mod.rs
@@ -0,0 +1,11 @@
+pub struct NineSlice {
+    pub _1: char,
+    pub _2: char,
+    pub _3: char,
+    pub _4: char,
+    pub _5: char,
+    pub _6: char,
+    pub _7: char,
+    pub _8: char,
+    pub _9: char,
+}
