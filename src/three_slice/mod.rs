ae44e5db9edeb64600632cc9a41f5a4f71c1a0d1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-06 23:47:33 +0200

    feat: update threeslice

--- src/three_slice/mod.rs
+++ src/three_slice/mod.rs
@@ -1,13 +1,25 @@
 pub struct ThreeSliceLine {
-    left_end: char,
-    center: char,
-    right_end: char,
+    pub _1: char,
+    pub _2: char,
+    pub _3: char,
 }
 
 impl ThreeSliceLine {
     pub fn render(&self, length: u32) -> String {
-        self.left_end.to_string()
-        + &self.center.to_string().repeat(length as usize)
-        + &self.right_end.to_string()
+        self.left_end().to_string()
+        + &self.center().to_string().repeat(length as usize)
+        + &self.right_end().to_string()
+    }
+
+    pub fn left_end(&self) -> char {
+        self._1
+    }
+
+    pub fn center(&self) -> char {
+        self._2
+    }
+
+    pub fn right_end(&self) -> char {
+        self._3
     }
 }
