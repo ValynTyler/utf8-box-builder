70b987ee8b1747ea3bb8d8d74a90acd84c140a42
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-06 23:54:29 +0200

    feat: nineslice getters

--- src/nine_slice/mod.rs
+++ src/nine_slice/mod.rs
@@ -9,3 +9,14 @@ pub struct NineSliceBox {
     pub _8: char,
     pub _9: char,
 }
+impl NineSliceBox {
+    pub fn top_left(&self)      -> char { self._1 }
+    pub fn top_middle(&self)    -> char { self._2 }
+    pub fn top_right(&self)     -> char { self._3 }
+    pub fn middle_left(&self)   -> char { self._4 }
+    pub fn center(&self)        -> char { self._5 }
+    pub fn middle_right(&self)  -> char { self._6 }
+    pub fn bottom_left(&self)   -> char { self._7 }
+    pub fn bottom_middle(&self) -> char { self._8 }
+    pub fn bottom_right(&self)  -> char { self._9 }
+}
